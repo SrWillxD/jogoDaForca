e9b521e9b1a6c827a6673b53e771fcce79b64f08
SrWillxD <willgra09@hotmail.com>
2023-04-25 18:50:35 -0300

    função inicializadora ponta, poximo passo sera a funcão clicado

--- script.js
+++ script.js
@@ -4,18 +4,28 @@ const palavras = {
     Objetos: [  "AMPULHETA",  "ANZOL",  "ALMOFARIZ",  "BOTIJA",  "CANDELABRO",  "DEDALEIRA",  "DESFIBRILADOR",  "ECHARPE",  "ESTRIBO",  "FAGOTE",  "FANTOCHE",  "FREEZER",  "NAVALHA",  "JALECO",  "MODEM",  "NEBULIZADOR",  "NOVELO",  "QUEPE",  "SELIM",  "SINTETIZADOR",  "SPRAY",  "URINOL",  "VUVUZELA",  "WEBCAM",  "XADREZ",  "XILOFONE"],
     Frutas: ["ALFARROBA", "BERGAMOTA", "CIRIGUELA", "CRANBERRY", "FEIJOA", "GROSELHA", "IMBU", "JENIPAPO", "KIWI", "LICHIA", "MEXERICA", "NECTARINA", "PEQUI", "PISTACHE", "POMELO", "SAGUARAJI"],
     Vulcões: ["PNEUMOULTRAMICROSCOPICOSSILICOVULCANOCONIOTICO", "LAVA", "QUENTE", "CINZA", "ROCHA", "CALOR", "DERRETIMENTO"], 
-    Cozinha: ["PANELAS", "COLHER", "RALADOR", "FRITADEIRA", "ELETRODOMESTICOS", "DEPURADOR", "CHAIRA"], 
-    
+    Cozinha: ["PANELAS", "COLHER", "RALADOR", "FRITADEIRA", "ELETRODOMESTICOS", "DEPURADOR", "CHAIRA", "TALHER"], 
+
 }
 
+init();
 
-function init() {
+function init(){
+    //* Seleciona uma categoria e uma palavra
     const valoresDasChavesDoObjeto = Object.keys(palavras);
     const grupo = valoresDasChavesDoObjeto[Math.floor(Math.random() * valoresDasChavesDoObjeto.length)];
     const palavra = palavras[grupo][Math.floor(Math.random() * palavras[grupo].length)];
 
+    //* Pega a categoria escolhida e usa como dica no HTML 
     document.getElementById("pDaDivDica").textContent = `A dica da palavra é: ${grupo}`;
+
+    //* Coloca a quantidade de underscores referente a quantidade de letras 
+    const underscore = palavra.replace(/\S/g, "___ ");
+    document.getElementById("divPalavraSecreta").textContent = underscore;
 
 
 }
-init();
+
+function clicado(){
+    
+}
