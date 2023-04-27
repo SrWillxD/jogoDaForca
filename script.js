532c705fb94758c2c22709fce56ebf43abf6c1f4
SrWillxD <willgra09@hotmail.com>
2023-04-27 18:06:41 -0300

    proxima alteração será implementar a função ganhou e perdeu

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
-//* Palavras a serem adivinhadas.
 const palavras = {
     Profissões: [    "APICULTOR",    "AUDITOR",    "BARTENDER",    "CERIMONIALISTA",    "CHEF",    "COACH",    "DESEMBARGADOR",    "DESPACHANTE",    "ENDOCRINOLOGISTA",    "EMBAIXADOR",    "HEADHUNTER",    "JUIZ",    "PIZZAIOLO",    "PERITO",    "QUIROPRATA",    "ROTEIRIZADOR",    "SILVICULTOR",    "TRADER"],
 
 }
 
-//* Inicializando a variavel fora pois preciso q ela esteja com o escopo global;
 let palavra="";
 let underscore="";
 
-//* Chamo a função inicial para escolher CATEGORIA, PALAVRA, definir a DICA e criar os UNDERSCORES.
 init();
 
 
 function init(){
-    //* Seleciona uma categoria e uma palavra
     const valoresDasChavesDoObjeto = Object.keys(palavras);
     const grupo = valoresDasChavesDoObjeto[Math.floor(Math.random() * valoresDasChavesDoObjeto.length)];
     palavra = palavras[grupo][Math.floor(Math.random() * palavras[grupo].length)];
 
-    //* Pega a categoria escolhida e usa como dica no HTML 
     document.getElementById("pDaDivDica").textContent = `A dica da palavra é: ${grupo}`;
 
-    //* Coloca a quantidade de underscores referente a quantidade de letras 
     underscore = palavra.replace(/\S/g, "— ");
     console.log(underscore.length);
     console.log(palavra.length);
     document.getElementById("divPalavraSecreta").textContent = underscore;
 }
 
-//* Capturo a div pai do teclado
 const divTeclado = document.getElementById('divTeclado');
 
-//* Cria uma variável para armazenar a tecla pressionada
 let teclaPressionada = '';
 
-//* Adicione um event listener a div pai do teclado
 divTeclado.addEventListener('click', function(event) {
-    //* Verifica se o elemento clicado é um botão
     if (event.target.nodeName === 'BUTTON') {
-    //* Atualiza a variável com a tecla pressionada
     teclaPressionada = event.target.value;
     let eventDoClique = event;
     verificaCertoErrado(teclaPressionada, palavra, eventDoClique);
@@ -51,15 +40,13 @@ function verificaCertoErrado(teclaPressionada, palavra, eventDoClique){
     if(palavra.includes(teclaPressionada)){
         acertou(teclaPressionada, palavra, eventDoClique);
     }else{
-        errou();
+        errou(eventDoClique);
     }
 }
 
 function acertou(teclaPressionada, palavra, eventDoClique) {
     let novaPalavraSecreta = palavra.replace(/\s/g, ""); 
     let novaPalavra = document.getElementById("divPalavraSecreta").textContent;
-
-
 
     for (let i = 0; i < novaPalavraSecreta.length; i++) {
         if (novaPalavraSecreta[i] == teclaPressionada) {
@@ -69,6 +56,7 @@ function acertou(teclaPressionada, palavra, eventDoClique) {
     
     document.getElementById("divPalavraSecreta").textContent = novaPalavra;
     coloreBotaoAcerto(eventDoClique);
+    piscaTelaVerde();
 }
 
 function coloreBotaoAcerto(eventDoClique){
@@ -76,8 +64,35 @@ function coloreBotaoAcerto(eventDoClique){
     
 }
 
-function errou(){
-    console.log("Não existe esta letra na palavra");
+function piscaTelaVerde() {
+    setTimeout(()=>{document.body.style.backgroundColor = "rgba(62, 255, 62, 0.500)"},1)
+    setTimeout(()=>{document.body.style.backgroundColor = "white"},200);
+}
 
+let numeroDaForca=0;
+function errou(eventDoClique){
+    let divImagem = document.getElementById('imagemForca');
+    numeroDaForca++;
+
+    if(numeroDaForca<6){
+        divImagem.src = `assets/img/imgsForca/forca${numeroDaForca}.png`;
+    }else{
+        divImagem.src = `assets/img/imgsForca/forca${numeroDaForca}.png`;
+        perdeu();
+    }
+
+    coloreBotaoErro(eventDoClique);
+    piscaTelaVermelho();
+}
+
+function coloreBotaoErro(eventDoClique){
+    eventDoClique.target.classList.add('btn-erro');
+
+}
+
+function piscaTelaVermelho() {
+    setTimeout(()=>{document.body.style.backgroundColor = "rgba(255, 0, 0, 0.486)"},1)
+    setTimeout(()=>{document.body.style.backgroundColor = "white"},200);
+    
 }
 
