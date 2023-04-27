1e8f72534021574c78b9ff9b7de7465bd64796d7
SrWillxD <willgra09@hotmail.com>
2023-04-27 15:28:14 -0300

    Correção da palavra secreta e cor nos botões clicados

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 //* Palavras a serem adivinhadas.
 const palavras = {
-    Vulcões: ["PNEUMOULTRAMICROSCOPICOSSILICOVULCANOCONIOTICO", "LAVA", "QUENTE", "CINZA", "ROCHA", "CALOR", "DERRETIMENTO"], 
-    Cozinha: ["PANELAS", "COLHER", "RALADOR", "FRITADEIRA", "ELETRODOMESTICOS", "DEPURADOR", "CHAIRA", "TALHER"], 
+    Profissões: [    "APICULTOR",    "AUDITOR",    "BARTENDER",    "CERIMONIALISTA",    "CHEF",    "COACH",    "DESEMBARGADOR",    "DESPACHANTE",    "ENDOCRINOLOGISTA",    "EMBAIXADOR",    "HEADHUNTER",    "JUIZ",    "PIZZAIOLO",    "PERITO",    "QUIROPRATA",    "ROTEIRIZADOR",    "SILVICULTOR",    "TRADER"],
 
 }
 
-//* Inicializando a variavel palavra fora pois preciso q ela esteja em com o escopo global;
+//* Inicializando a variavel fora pois preciso q ela esteja com o escopo global;
 let palavra="";
+let underscore="";
 
 //* Chamo a função inicial para escolher CATEGORIA, PALAVRA, definir a DICA e criar os UNDERSCORES.
 init();
+
 
 function init(){
     //* Seleciona uma categoria e uma palavra
@@ -21,8 +22,9 @@ function init(){
     document.getElementById("pDaDivDica").textContent = `A dica da palavra é: ${grupo}`;
 
     //* Coloca a quantidade de underscores referente a quantidade de letras 
-    const underscore = palavra.replace(/\S/g, "— ");
-    console.log(underscore);
+    underscore = palavra.replace(/\S/g, "— ");
+    console.log(underscore.length);
+    console.log(palavra.length);
     document.getElementById("divPalavraSecreta").textContent = underscore;
 }
 
@@ -34,34 +36,48 @@ let teclaPressionada = '';
 
 //* Adicione um event listener a div pai do teclado
 divTeclado.addEventListener('click', function(event) {
-  //* Verifica se o elemento clicado é um botão
+    //* Verifica se o elemento clicado é um botão
     if (event.target.nodeName === 'BUTTON') {
     //* Atualiza a variável com a tecla pressionada
     teclaPressionada = event.target.value;
-    verificaCertoErrado(teclaPressionada, palavra);
+    let eventDoClique = event;
+    verificaCertoErrado(teclaPressionada, palavra, eventDoClique);
     }
 });
 
 
 
-function verificaCertoErrado(teclaPressionada, palavra){
-    //TODO verificar se a palavra contem a tecla q foi clicada
+function verificaCertoErrado(teclaPressionada, palavra, eventDoClique){
     if(palavra.includes(teclaPressionada)){
-        acertou(teclaPressionada, palavra);
+        acertou(teclaPressionada, palavra, eventDoClique);
     }else{
         errou();
     }
-    //TODO Se a palavra conter a letra, chamar a função acertou()
-
-    //TODO Se não tiver, chamar a função errou()
 }
 
-function acertou(){
-    //TODO Substituit os tracinhos pela letra em todas as ocorrencias da palavra
-    console.log("Boa");
+function acertou(teclaPressionada, palavra, eventDoClique) {
+    let novaPalavraSecreta = palavra.replace(/\s/g, ""); 
+    let novaPalavra = document.getElementById("divPalavraSecreta").textContent;
+
+
+
+    for (let i = 0; i < novaPalavraSecreta.length; i++) {
+        if (novaPalavraSecreta[i] == teclaPressionada) {
+            novaPalavra = novaPalavra.substring(0, 2 * i) + teclaPressionada + novaPalavra.substring(2 * i + 1);
+        }
+    }
+    
+    document.getElementById("divPalavraSecreta").textContent = novaPalavra;
+    coloreBotaoAcerto(eventDoClique);
+}
+
+function coloreBotaoAcerto(eventDoClique){
+    eventDoClique.target.classList.add('btn-acerto');
+    
 }
 
 function errou(){
     console.log("Não existe esta letra na palavra");
 
 }
+
