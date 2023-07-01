a7c2ee10d5d1d1ee5773355d547d522e8340345a
SrWillxD <willgra09@hotmail.com>
2023-06-30 21:38:57 -0300

    refatoração feita para limpeza do código, nenhuma lógica foi alterada

--- script.js
+++ script.js
@@ -4,17 +4,24 @@ let wordToBeGuessed="";
 
 const divSecretWord = document.getElementById("divSecretWord");
 
-function startTheGame(){
+function startTheGame(){ 
     const wordGroup = defineWordGroup();
     tipOfWord(wordGroup);
     wordToBeGuessed = defineGroupWord(wordGroup);
-    dashesThatHideTheWord(wordToBeGuessed); 
+    console.log(wordToBeGuessed);//!Remover
+    dashesThatHideTheWord(wordToBeGuessed);
 }
+
 startTheGame();
 
-function defineWordGroup(){
+function defineWordGroup(){ 
     const group = Object.keys(wordObject)[Math.floor(Math.random() * Object.keys(wordObject).length)];
     return group;
+}
+
+function tipOfWord(wordGroup){
+    const pFromDivTip = document.getElementById("pFromDivTip");
+    pFromDivTip.textContent = `A dica da palavra é: ${wordGroup}`;
 }
 
 function defineGroupWord(group){
@@ -22,10 +29,6 @@ function defineGroupWord(group){
     return wordToBeGuessed;
 }
 
-function tipOfWord(wordGroup){
-    const pFromDivTip = document.getElementById("pFromDivTip");
-    pFromDivTip.textContent = `A dica da palavra é: ${wordGroup}`;
-}
 
 function dashesThatHideTheWord(wordToBeGuessed){
     const dashesThatHideTheWord = wordToBeGuessed.replace(/\S/g, "— ");
@@ -35,19 +38,18 @@ function dashesThatHideTheWord(wordToBeGuessed){
 let keyPressedValue = '';
 
 const divKeyboard = document.getElementById('divKeyboard');
+
 divKeyboard.addEventListener('click', (event)=>{
-    if (event.target.nodeName === 'BUTTON'){
     keyPressedValue = event.target.value;
     let clickEvent = event;
     checkIfContainsLetter(clickEvent);
-    }
 });
 
 function checkIfContainsLetter(clickEvent){
     if(wordToBeGuessed.includes(keyPressedValue)){
         contains(clickEvent);
     }else{
-        NotContains(clickEvent);
+        notContains(clickEvent);
     }
 }
 
@@ -60,15 +62,14 @@ function contains(clickEvent){
 }
 
 function UpdatesTheWordWithEveryKeystroke(){
-    let wordToBeGuessedWithoutSpaces = wordToBeGuessed.replace(/\s/g, ""); 
     let wordUpdatedEveryKeystroke = divSecretWord.textContent;
-    
-    for (let i = 0; i < wordToBeGuessedWithoutSpaces.length; i++){
-        if(wordToBeGuessedWithoutSpaces[i] == keyPressedValue) {
+
+    for (let i = 0; i < wordToBeGuessed.length; i++){
+        if(wordToBeGuessed[i] == keyPressedValue) {
             wordUpdatedEveryKeystroke = wordUpdatedEveryKeystroke.substring(0, 2 * i) + keyPressedValue + wordUpdatedEveryKeystroke.substring(2 * i + 1);
         }
     }
-    
+
     divSecretWord.textContent = wordUpdatedEveryKeystroke;
     return wordUpdatedEveryKeystroke;
 }
@@ -78,7 +79,7 @@ function colorTheButtonGreen(clickEvent){
 }
 
 function flashGreenScreen(){
-    setTimeout(()=>{document.body.style.backgroundColor = "rgba(62, 255, 62, 0.500)"},1);
+    document.body.style.backgroundColor = "rgba(62, 255, 62, 0.500)";
     setTimeout(()=>{document.body.style.backgroundColor = "grey"},200);
 }
 
@@ -86,7 +87,7 @@ let overlay = document.querySelector('.overlay');
 let notification = document.querySelector('.notification');
 
 function checkIfWon(wordUpdatedEveryKeystroke){
-    if(!wordUpdatedEveryKeystroke.includes("— ")){
+    if(!wordUpdatedEveryKeystroke.includes("—")){
         setTimeout(()=> {displayNotificationWon()}, 600);
     }
     
@@ -98,14 +99,14 @@ function displayNotificationWon(){
 }
 
 let gallowsNumber=0;
-function NotContains(clickEvent){
+function notContains(clickEvent){
     if(gallowsNumber<6){
         gallowsNumber++
     }
     
     updateGallowsPhoto();
     colorTheButtonRed(clickEvent);
-    flashGreenRed();
+    flashScreenRed();
     checkIfLost();
 }
 
@@ -118,8 +119,8 @@ function colorTheButtonRed(clickEvent){
     clickEvent.target.classList.add('wrongButton');
 }
 
-function flashGreenRed(){
-    setTimeout(()=>{document.body.style.backgroundColor = "rgba(255, 0, 0, 0.300)"},1);
+function flashScreenRed(){
+    document.body.style.backgroundColor = "rgba(255, 0, 0, 0.300)";
     setTimeout(()=>{document.body.style.backgroundColor = "grey"},200);
 }
 
@@ -130,14 +131,12 @@ function checkIfLost(){
 }
 
 function displayNotificationLost(){
-    notification.querySelector("p").textContent= `A palavra era: ${wordToBeGuessed}. Você errou.`;
+    notification.querySelector("p").textContent= `A palavra era: ${wordToBeGuessed}. Você perdeu.`;
     overlay.style.display = 'block';
     notification.style.display = 'block';
 }
 
-const resetGameButton = document.querySelectorAll(".restartGame");
-for (let i = 0; i < resetGameButton.length; i++){
-    resetGameButton[i].addEventListener("click", ()=>{
+const resetGameButton = document.querySelector(".restartGame");
+resetGameButton.addEventListener("click", ()=>{
     location.reload();
-    });
-}
+});
